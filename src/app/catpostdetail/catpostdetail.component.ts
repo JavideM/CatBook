afb57d870921859f801868b830db93b50a0331a9
Francisco Javier de Martin Rivares <fjdmartin@grupotr.es>
2018-04-23 16:30:15 +0200

    added bootsrap

--- src/app/catpostdetail/catpostdetail.component.ts
+++ src/app/catpostdetail/catpostdetail.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit, Input } from '@angular/core';
 @Component({
   selector: 'app-catpostdetail',
   templateUrl: './catpostdetail.component.html',
-  styleUrls: ['./catpostdetail.component.css','../../../node_modules/bootstrap-css-only/css/bootstrap.min.css']
+  styleUrls: ['./catpostdetail.component.css','../../../node_modules/bootstrap/dist/css/bootstrap.min.css']
 })
 export class CatpostdetailComponent implements OnInit {
   @Input() cat: string;
